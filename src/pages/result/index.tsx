32314bfcd6fc08ab45fa7d41f06e1d81b9c80a14
MaxtuneLee <60775796+MaxtuneLee@users.noreply.github.com>
2022-03-19 19:44:12 +0800

    feat: 结果页

--- src/pages/result/index.tsx
+++ src/pages/result/index.tsx
@@ -1,17 +1,33 @@
 import { View, Text, Button, Image } from '@tarojs/components';
 import Taro from '@tarojs/taro';
-import React from 'react';
+import React, { useState } from 'react';
 import resultImg from '../../imgs/result.png'
+import express from '../../imgs/onrail.png'
 import './index.sass'
 
+function Imgs(cases){
+  switch (cases){
+    case 'order': return resultImg;
+    case 'express': return express;
+  }
+}
+
+function notice(cases){
+  switch (cases){
+    case 'order': return '支付成功，登记已提交';
+    case 'express': return '你的 T-Shirt 已经在路上啦';
+  }
+}
+
 function Result() {
+const [cases, setCases]=useState('express');
   return (
     <View className='wrapper'>
       <Image
-        src={resultImg}
+        src={Imgs(cases)}
         className='logo'
       ></Image>
-      <Text className='title'>支付成功，登记已提交</Text>
+      <Text className='title'>{notice(cases)}</Text>
       <Button type='default' className='back' onClick={()=>{
         Taro.navigateTo({url:'/pages/form/index'})
       }}
